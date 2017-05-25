a179b12fff4baabe7e8f4f8de1e7667fd92daf89
ssveg <ssveg@gmail.com>
2017-05-25 00:55:23 -0700

    Fixed bugs, program fully working now'

--- SZ_ALPHA-dist/Overpopulation/Overpopulation_SVG_VIS_FOR_BRIFL.py
+++ SZ_ALPHA-dist/Overpopulation/Overpopulation_SVG_VIS_FOR_BRIFL.py
@@ -12,7 +12,7 @@ W = 500
 H = 400
 
 def render_state(s):
-    dwg = swgwrite.Drawing(filename = "test-svgwrite.svg",
+    dwg = svgwrite.Drawing(filename = "test-svgwrite.svg",
                            id = "state_svg",
                            size = (str(W) + "px", str(H) + "px"),
                            debug = True)
@@ -23,7 +23,7 @@ def render_state(s):
                      stroke = "black",
                      fill = "rgb(100, 255, 0)"))
 
-    dwg.add(dwg.text("Population: " + str(s['POP_COUNT']) + ", Rate: " + str(s['GROWTH_RATE']),
+    dwg.add(dwg.text("Population: " + str(s.pop_count) + ", Rate: " + str(s.growth_rate),
                      insert = (W / 2, H / 2),
                      text_anchor = "middle",
                      font_size="25",
@@ -33,6 +33,6 @@ def render_state(s):
 
 if __name__ == '__main__':
     DEBUG = True
-    INITIAL_STATE = {'POP_COUNT':500, 'GROWTH_RATE':1.185}
+    INITIAL_STATE = (1.185, 500)
     print(INITIAL_STATE)
     render_state(INITIAL_STATE)
