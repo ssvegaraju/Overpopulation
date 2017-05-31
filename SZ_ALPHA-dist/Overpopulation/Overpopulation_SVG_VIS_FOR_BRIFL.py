eace2ce9d952642dcfefa9d25ccdd8cbcfeb7500
ssveg <ssveg@gmail.com>
2017-05-30 22:24:47 -0700

    visualization update

--- SZ_ALPHA-dist/Overpopulation/Overpopulation_SVG_VIS_FOR_BRIFL.py
+++ SZ_ALPHA-dist/Overpopulation/Overpopulation_SVG_VIS_FOR_BRIFL.py
@@ -4,7 +4,7 @@
 # Python version 3.6.1
 
 import svgwrite
-from Overpopulation import POP_COUNT, GROWTH_RATE
+from Overpopulation import POP_COUNT, GROWTH_RATE, WEALTH
 
 DEBUG = False
 
@@ -21,13 +21,46 @@ def render_state(s):
                      size = (str(W) + "px", str(H) + "px"),
                      stroke_width = "1",
                      stroke = "black",
-                     fill = "rgb(100, 255, 0)"))
+                     fill = "rgb(100, 100, 100)")) #grey
 
-    dwg.add(dwg.text("Population: " + str(s.pop_count) + ", Rate: " + str(s.growth_rate),
-                     insert = (W / 2, H / 2),
-                     text_anchor = "middle",
-                     font_size="25",
-                     fill = "red"))
+    # Wealth bar
+    dwg.add(dwg.rect(insert = (0, 10),
+                     size = (str(s.wealth * 5) + "px", str(H / 10) + "px"),
+                     stroke_width = "1",
+                     stroke = "black",
+                     fill = "rgb(50, 50, 255)"))
+
+    dwg.add(dwg.text("Wealth = " + str(s.wealth),
+                     insert = (1, 10 + (H / 20)),
+                     text_anchor = "start",
+                     font_size="12",
+                     fill="white"))
+
+    # Population bar
+    dwg.add(dwg.rect(insert = (0, 130),
+                     size = (str(s.pop_count / 50) + "px", str(H / 10) + "px"),
+                     stroke_width = "1",
+                     stroke = "black",
+                     fill = "rgb(50, 50, 255)"))
+
+    dwg.add(dwg.text("Population count = " + str(s.pop_count),
+                     insert = (1, 130 + (H / 20)),
+                     text_anchor = "start",
+                     font_size="12",
+                     fill="white"))
+
+    # Growth rate bar
+    dwg.add(dwg.rect(insert = (0, 260),
+                     size = (str(s.growth_rate * 25) + "px", str(H / 10) + "px"),
+                     stroke_width = "1",
+                     stroke = "black",
+                     fill = "rgb(50, 50, 255)"))
+
+    dwg.add(dwg.text("Growth rate = " + str(s.growth_rate),
+                     insert = (1, 260 + (H / 20)),
+                     text_anchor = "start",
+                     font_size="12",
+                     fill="white"))
 
     return (dwg.tostring())
 
