753e07ab47a2763d302d4bc50b8d1b09e30037ec
ssveg <ssveg@gmail.com>
2017-05-30 22:34:24 -0700

    Game ending now has message

--- SZ_ALPHA-dist/Overpopulation/Overpopulation_SVG_VIS_FOR_BRIFL.py
+++ SZ_ALPHA-dist/Overpopulation/Overpopulation_SVG_VIS_FOR_BRIFL.py
@@ -4,7 +4,7 @@
 # Python version 3.6.1
 
 import svgwrite
-from Overpopulation import POP_COUNT, GROWTH_RATE, WEALTH
+from Overpopulation import POP_COUNT, GROWTH_RATE, WEALTH, goal_test
 
 DEBUG = False
 
@@ -23,44 +23,52 @@ def render_state(s):
                      stroke = "black",
                      fill = "rgb(100, 100, 100)")) #grey
 
-    # Wealth bar
-    dwg.add(dwg.rect(insert = (0, 10),
-                     size = (str(s.wealth * 5) + "px", str(H / 10) + "px"),
-                     stroke_width = "1",
-                     stroke = "black",
-                     fill = "rgb(50, 50, 255)"))
+    if not goal_test(s):
+        # Wealth bar
+        dwg.add(dwg.rect(insert = (0, 10),
+                         size = (str(s.wealth * 5) + "px", str(H / 10) + "px"),
+                         stroke_width = "1",
+                         stroke = "black",
+                         fill = "rgb(50, 50, 255)"))
 
-    dwg.add(dwg.text("Wealth = " + str(s.wealth),
-                     insert = (1, 10 + (H / 20)),
-                     text_anchor = "start",
-                     font_size="12",
-                     fill="white"))
+        dwg.add(dwg.text("Wealth = " + str(s.wealth),
+                         insert = (1, 10 + (H / 20)),
+                         text_anchor = "start",
+                         font_size="12",
+                         fill="white"))
 
-    # Population bar
-    dwg.add(dwg.rect(insert = (0, 130),
-                     size = (str(s.pop_count / 50) + "px", str(H / 10) + "px"),
-                     stroke_width = "1",
-                     stroke = "black",
-                     fill = "rgb(50, 50, 255)"))
+        # Population bar
+        dwg.add(dwg.rect(insert = (0, 130),
+                         size = (str(s.pop_count / 50) + "px", str(H / 10) + "px"),
+                         stroke_width = "1",
+                         stroke = "black",
+                         fill = "rgb(50, 50, 255)"))
 
-    dwg.add(dwg.text("Population count = " + str(s.pop_count),
-                     insert = (1, 130 + (H / 20)),
-                     text_anchor = "start",
-                     font_size="12",
-                     fill="white"))
+        dwg.add(dwg.text("Population count = " + str(s.pop_count),
+                         insert = (1, 130 + (H / 20)),
+                         text_anchor = "start",
+                         font_size="12",
+                         fill="white"))
 
-    # Growth rate bar
-    dwg.add(dwg.rect(insert = (0, 260),
-                     size = (str(s.growth_rate * 25) + "px", str(H / 10) + "px"),
-                     stroke_width = "1",
-                     stroke = "black",
-                     fill = "rgb(50, 50, 255)"))
+        # Growth rate bar
+        dwg.add(dwg.rect(insert = (0, 260),
+                         size = (str(s.growth_rate * 25) + "px", str(H / 10) + "px"),
+                         stroke_width = "1",
+                         stroke = "black",
+                         fill = "rgb(50, 50, 255)"))
 
-    dwg.add(dwg.text("Growth rate = " + str(s.growth_rate),
-                     insert = (1, 260 + (H / 20)),
-                     text_anchor = "start",
-                     font_size="12",
-                     fill="white"))
+        dwg.add(dwg.text("Growth rate = " + str(s.growth_rate),
+                         insert = (1, 260 + (H / 20)),
+                         text_anchor = "start",
+                         font_size="12",
+                         fill="white"))
+
+    else:
+        dwg.add(dwg.text("The Simulation has Concluded, thanks for playing!",
+                insert = (W / 2, H / 2),
+                text_anchor = "middle",
+                font_size = "20",
+                fill="white"))
 
     return (dwg.tostring())
 
