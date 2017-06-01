c412904e9188b14b290b3c761a8994b7610d763b
ssveg <ssveg@gmail.com>
2017-06-01 01:02:58 -0700

    Some operators can be applied only once

--- SZ_ALPHA-dist/Overpopulation/Overpopulation.py
+++ SZ_ALPHA-dist/Overpopulation/Overpopulation.py
@@ -27,25 +27,29 @@ Overpopulation.
 
 #<COMMON_DATA>
 #</COMMON_DATA>
-GROWTH_RATE = 1.185
-POP_COUNT = 1000
-WEALTH = 100
-YEAR = 2000
+GROWTH_RATE = 1.185      
+POP_COUNT = 1000     
+WEALTH = 100    
+YEAR = 2000    
+USED = 0                                                                                            #index of operator
+USED_INDEX = [False, False, False, False, False, False, False, False, False, False, False, False]   #Checks whether an operator has been used or not
 #<COMMON_CODE>
 
 class State:
-    def __init__(self, growth_rate, pop_count, wealth, year):
+    def __init__(self, growth_rate, pop_count, wealth, year, used):
         self.growth_rate = growth_rate
         self.pop_count = pop_count
         self.wealth = wealth
         self.year = year
+        self.used = used
 
     def __copy__(self):
-        news = State(None, None, None, None)
+        news = State(None, None, None, None, None)
         news.growth_rate = self.growth_rate
         news.pop_count = self.pop_count
         news.wealth = self.wealth
         news.year = self.year
+        news.used = self.used
         return news
 
     def __str__(self):
@@ -57,21 +61,27 @@ class State:
         if self.pop_count != s.pop_count: return False
         if self.wealth != s.wealth: return False
         if self.year != s.year: return False
+        if self.used != s.used: return False
         return True
 
     def __hash__(self):
         return (self.__str__()).__hash__()
 
-def can_apply(state, role_number):
+def can_apply(state, role_number, used):
+    s = state.__copy__()
+    if (used != None):
+        return not USED_INDEX[used] #if the index for the operator is not null, check array to see if it has been used before
     return not goal_test(state)
 
-def apply_op(state, growth_factor, cost):
+def apply_op(state, growth_factor, cost, used):
     s = state.__copy__()
     s.growth_rate -= growth_factor
     s.pop_count *= s.growth_rate
     s.pop_count = round(s.pop_count)
     s.wealth -= cost
     s.year += 1
+    if (used != None):
+        USED_INDEX[used] = True     #similar check as above, just setting it to true if it hasnt been used before
     return s
 
 def goal_test(s):
@@ -81,23 +91,24 @@ def goal_message(s):
     return "The Simulation has Concluded"
 
 class Operator:
-    def __init__(self, name, precond, state_transf, growth_rate, cost):
+    def __init__(self, name, precond, state_transf, growth_rate, cost, used):
         self.precond = precond
         self.state_transf = state_transf
         self.growth_rate = growth_rate
         self.cost = cost
         self.name = name + " Growth Rate: " + str(self.growth_rate) + ", Cost: $" + str(self.cost)
+        self.used = used
 
     def is_applicable(self, s, role_number):
         return self.precond(s, role_number)
 
     def apply(self, s):
-        return self.state_transf(s, self.growth_rate, self.cost)
+        return self.state_transf(s, self.growth_rate, self.cost, self.used)
 
 #</COMMON_CODE>
 
 #<INITIAL_STATE>
-INITIAL_STATE = State(GROWTH_RATE, POP_COUNT, WEALTH, YEAR)
+INITIAL_STATE = State(GROWTH_RATE, POP_COUNT, WEALTH, YEAR, USED)
 print(INITIAL_STATE)
 #</INITIAL_STATE>
 
@@ -107,30 +118,30 @@ ROLES = [{'name':'Gov. Official', 'min':1, 'max':1}]
 
 #<OPERATORS>
 OPERATORS = [Operator("Require SexEd in Schools.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0015, 5),
+                      lambda s, v: can_apply(s,v,0), lambda s, g, c, u: apply_op(s, g, c, u), 0.0015, 5, 0),
              Operator("Support Planned Parenthood.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0015, 5),
+                      lambda s, v: can_apply(s,v,None), lambda s, g, c, u: apply_op(s, g, c, u), 0.0015, 5, None),
              Operator("Increase investment in technology sector of domestic economy.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0015, 5),
+                      lambda s, v: can_apply(s,v,None), lambda s, g, c, u: apply_op(s, g, c, u), 0.0015, 5, None),
              Operator("One-Child Policy.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.02, 5),
+                      lambda s, v: can_apply(s,v,3), lambda s, g, c, u: apply_op(s, g, c, u), 0.02, 5, 3),
              Operator("Universal access to safe contraceptives.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.01, 5),
+                      lambda s, v: can_apply(s,v,4), lambda s, g, c, u: apply_op(s, g, c, u), 0.01, 5, 4),
              Operator("Guarantee secondary education, especially for girls.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0005, 5),
+                      lambda s, v: can_apply(s,v,5), lambda s, g, c, u: apply_op(s, g, c, u), 0.0005, 5, 5),
              Operator("Eradicate gender bias from law, economic opportunity, health, and culture.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0025, 5),
+                      lambda s, v: can_apply(s,v,6), lambda s, g, c, u: apply_op(s, g, c, u), 0.0025, 5, 6),
              Operator("End policies that offer financial incentives based on number of children.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0015, 5),
+                      lambda s, v: can_apply(s,v,7), lambda s, g, c, u: apply_op(s, g, c, u), 0.0015, 5, 7),
              Operator("Stress education on population, environment, and development.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0015, 5),
+                      lambda s, v: can_apply(s,v,None), lambda s, g, c, u: apply_op(s, g, c, u), 0.0015, 5, None),
              Operator("Put prices on environmental costs/impacts.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.005, 5),
+                      lambda s, v: can_apply(s,v,None), lambda s, g, c, u: apply_op(s, g, c, u), 0.005, 5, None),
              Operator("Promote transition from childbearing population to an aging population.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0015, 5),
+                      lambda s, v: can_apply(s,v,None), lambda s, g, c, u: apply_op(s, g, c, u), 0.0015, 5, None),
              Operator("Commit to stabilizing population growth through the\
                        exercise of human rights and development.",
-                      lambda s, v: can_apply(s,v), lambda s, g, c: apply_op(s, g, c), 0.0015, 5)]
+                      lambda s, v: can_apply(s,v,None), lambda s, g, c, u: apply_op(s, g, c, u), 0.0015, 5, None)]
 
 #</OPERATORS>
 
@@ -148,4 +159,4 @@ render_state = None
 def use_BRIFL_SVG():
     global render_state
     from Overpopulation_SVG_VIS_FOR_BRIFL import render_state
-#</STATE_VIS>  
+#</STATE_VIS>          
